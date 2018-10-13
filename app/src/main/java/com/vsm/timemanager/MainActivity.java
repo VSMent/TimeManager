82b6fec1b9705890b69ee03f3017668466a36ed5
Vasyl <vasylko1999ko@gmail.com>
2018-10-13 23:50:15 +0300

    Working simple notification (not sure about Oreo and up)

--- app/src/main/java/com/vsm/timemanager/MainActivity.java
+++ app/src/main/java/com/vsm/timemanager/MainActivity.java
@@ -1,13 +1,58 @@
 package com.vsm.timemanager;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+//        Service
+//        startService();   // Start service and notification
+
+//        Notification
+        createNotificationChannel();
+        Notification myNotification = new NotificationCompat.Builder(this, getString(R.string.channel_id))
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("App was turned on")
+                .setContentText("This is first time application was turned on.\nAnd of course it was cosed")
+//                .setStyle(new NotificationCompat.BigTextStyle()
+//                        .bigText("Much longer text that cannot fit one line..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .build();
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(1, myNotification);
+
+//        Log.d("Test", "Worked");
+        finish();
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(getString(R.string.channel_id), name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+
+        }
     }
 }
